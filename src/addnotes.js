8d1b4241eac190ef1c07d9979196ad285b04883b
Tony Lauf <LAU5619@tonys-mbp.nm.nmfco.com>
2018-02-13 16:42:43 -0600

    sort by added and working, need to add voting and note details

--- src/addnotes.js
+++ src/addnotes.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import { postNote } from './state/actions';
 import axios from 'axios';
 
-class Battle extends Component {
+class AddNotes extends Component {
   constructor(props) {
     super(props);
 
@@ -23,6 +23,8 @@ class Battle extends Component {
     this.onNoteClick = this.onNoteClick.bind(this)
     this.postNote = this.postNote.bind(this)
     this.getNotes = this.getNotes.bind(this)
+    this.sortNotes = this.sortNotes.bind(this)
+    this.sortBy = this.sortBy.bind(this)
   }
 
   componentDidMount() {
@@ -52,7 +54,7 @@ class Battle extends Component {
     let apiVal = `http://5a8318ed98bd81001246c8e1.mockapi.io/anonnote/v1/notes`
     axios.post(apiVal, payload)
       .then((response) => {
-        console.log(response.data)
+        this.setState({ noteVal: '' })
         this.getNotes()
         // if (payload.id == 1) {
         //   this.setState({ noteVal: '', noteSbmBtn: true, noteSuccess: true, notePublicRepos: response.data.public_repos, noteFollowers: response.data.followers, noteAvatarURL: response.data.avatar_url })
@@ -75,13 +77,33 @@ class Battle extends Component {
     let apiVal = `http://5a8318ed98bd81001246c8e1.mockapi.io/anonnote/v1/notes`
     axios.get(apiVal)
       .then((response) => {
-        console.log(response.data)
         this.setState({ notes: response.data, notesExist: true, noteErr: false, noteMsg: '' })
+        this.sortNotes('sortByDate')
       })
       .catch((error) => {
         console.log(error)
         this.setState({ notesExist: false, noteErr: true, noteMsg: 'Failed to get notes.' })
       })
+  }
+
+  sortBy(evt) {
+    this.sortNotes(evt.target.id)
+  }
+
+  sortNotes(sortBy) {
+    let sortedNotes = this.state.notes.slice()
+    if (sortBy === 'sortByDate') {
+      sortedNotes.sort(function (a, b) {
+        return b.createdAt - a.createdAt;
+      })
+    } else {
+      if (sortBy === 'sortByVote') {
+        sortedNotes.sort(function (a, b) {
+          return a.createdAt - b.createdAt;
+        })
+      }
+    }
+    this.setState({ notes: sortedNotes })
   }
 
   render() {
@@ -90,7 +112,7 @@ class Battle extends Component {
         <header className="App-header">
           <h1 className="App-title">{this.props.title}</h1>
         </header>
-        <form>
+        <form onSubmit={this.onNoteClick}>
           <div className="row">
             <div className="small-12 columns">
               <div className="card">
@@ -105,28 +127,56 @@ class Battle extends Component {
               </div>
             </div>
             {this.state.notesExist &&
-              <div className="row">
-                <div className="small-12 columns">
-                  <div className="card">
-                    <div className="noteCtn">
-                      {this.state.notes.map((note, idx) => {
-                        let extDate = new Date(note.createdAt * 1000)
-                        let dspMonth = extDate.toLocaleString("en", { month: "short" })
-                        let dspDay = extDate.getDate()
-                        let dspYear = extDate.getFullYear()
-                        let dspHour = extDate.getHours()
-                        let dspMins = extDate.getMinutes()
-                        let dspSecs = extDate.getSeconds()
-                        return (
-                          <div className="row">
-                            <div className="small-9 columns">&nbsp;</div>
-                            <div className="small-3 columns noteDsp">Noted on: {dspMonth} {dspDay}, {dspYear}, {dspHour}:{dspMins}:{dspSecs} </div>
-                            <div className="row"></div>
-                            <div className="small-12 columns noteDsp" key={idx}>{note.note} </div>
-                          </div>
-                      )
-                      })
-                      }
+              <div>
+                <div className="row padding-medium">
+                  <div className="small-12 columns">
+                    <div className="card">
+                      <div className="row">
+                        <div className="small-8 columns">&nbsp;</div>
+                        <div className="small-1 columns">
+                          <div>Sort By:</div>
+                        </div>
+                        <input type="button" className="button srtBtn" id="sortByDate" onClick={this.sortBy} value="Date" />
+                        <input type="button" className="button srtBtn" id="sortByVote" onClick={this.sortBy} value="Vote" />
+                        <div className="small-3 columns">&nbsp;</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row padding-medium">
+                  <div className="small-12 columns">
+                    <div className="card">
+                      <div className="noteCtn">
+                        <table className="table scrollable" summary="This summary is for screen readers and should summarize the structure of the table headers and rows">
+                          <caption className="show-for-sr"></caption>
+                          <tbody className="tbody">
+                            <thead>
+                              <tr>
+                                <th width="900">Note</th>
+                                <th width="100">Created</th>
+                              </tr>
+                            </thead>
+                            {this.state.notes.map((note, idx) => {
+                              let extDate = new Date(note.createdAt * 1000)
+                              let dspMonth = extDate.toLocaleString("en", { month: "short" })
+                              let dspDay = extDate.getDate(), dspYear = extDate.getFullYear(), dspHour = extDate.getHours(), dspMins = extDate.getMinutes(), dspSecs = extDate.getSeconds(), dspAMPM = (dspHour >= 12) ? "PM" : "AM";
+                              return (
+                                //     <div className="row" key={idx}>
+                                //       <div className="small-9 columns">&nbsp;</div>
+                                //       <div className="small-3 columns noteDsp">Noted on: {dspMonth} {dspDay}, {dspYear}, {dspHour}:{dspMins}:{dspSecs} {dspAMPM} </div>
+                                //       <div className="row"></div>
+                                //       <div className="small-12 columns noteDsp">{note.note} </div>
+                                //     </div>
+                                <tr key={idx}>
+                                  <td>{note.note}</td>
+                                  <td>Noted on: {dspMonth} {dspDay}, {dspYear}, {dspHour}:{dspMins}:{dspSecs} {dspAMPM}</td>
+                                </tr>
+                              )
+                            })
+                            }
+                          </tbody>
+                        </table>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -153,4 +203,4 @@ const mapDispatchToProps = (dispatch) => {
     },
   }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(Battle);
+export default connect(mapStateToProps, mapDispatchToProps)(AddNotes);
